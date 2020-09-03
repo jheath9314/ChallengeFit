b51e44b1848457e00db1b979ad330a85df49d001
Vladimir Novikov <vzn51@psu.edu>
2020-09-02 20:59:48 -0400

    Add project files.

---
+++ SWENG894/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SWENG894.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
