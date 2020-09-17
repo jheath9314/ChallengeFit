989e3d3706245b17761c993663cd97179e7cd7b3
jheath9314 <35749255+jheath9314ou@users.noreply.github.com>
2020-09-16 21:21:20 -0600

    added JS to handle label changes when changing workout scoring

--- SWENG894/wwwroot/js/site.js
+++ SWENG894/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+
+function handleScoringTypeChange() {
+    
+    $(document).on('change', '#scoringType', function () {
+        var label = $('#scoringType').find(":selected").text();
+        $('#timeLabel').text(label);
+    });
+}
