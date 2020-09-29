a38604363ef583163dacf7b64a3fd9f2920b7011
Vladimir Novikov <vzn51@psu.edu>
2020-09-29 17:47:22 -0400

    Clean migrations.

--- SWENG894/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ SWENG894/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -3,17 +3,15 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SWENG894.Data;
 
-namespace SWENG894.Migrations
+namespace SWENG894.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20200926193201_Intial")]
-    partial class Intial
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -362,9 +360,6 @@ namespace SWENG894.Migrations
                         .HasColumnType("nvarchar(450)");
 
                     b.Property<int>("WorkoutId")
-                        .HasColumnType("int");
-
-                    b.Property<int>("temp")
                         .HasColumnType("int");
 
                     b.HasKey("Id");
