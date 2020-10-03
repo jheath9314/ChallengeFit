8362b59394a63feb49a8e61a2c3f51f4358ce6d5
Stephen Cook <sxc1611@psu.edu>
2020-10-03 15:55:10 +0000

    Added FullNameIsValid() and FullNameIsNotNull() to ApplicationUserTest.cs

--- SWENG894.Test/Models/ApplicationUserTest.cs
+++ SWENG894.Test/Models/ApplicationUserTest.cs
@@ -20,5 +20,46 @@ namespace SWENG894.Test
             Assert.Equal("53202", appUser.ZipCode);
             Assert.Equal("User",  appUser.Role);
         }
+        
+        //FullName
+        //tests FullName assignent is valid
+        [Fact]
+        public void FullNameIsValid()
+        {
+            //arrange
+            ApplicationUser appUser = new ApplicationUser();
+            appUser.FirstName = "John";
+            appUser.LastName = "Doe";
+            appUser.ZipCode = "53202";
+            appUser.Role = "User";
+
+            //act
+            var FullName = appUser.FullName;
+
+            //assert
+            Assert.Equal("John Doe", FullName);
+            
+        }
+        
+        //tests FullName assignent is NOT null
+        [Fact]
+        public void FullNameIsNotNull()
+        {
+            //arrange
+            ApplicationUser appUser = new ApplicationUser();
+            appUser.FirstName = "John";
+            appUser.LastName = "Doe";
+            appUser.ZipCode = "53202";
+            appUser.Role = "User";
+
+            //act
+            var FullName = appUser.FullName;
+
+            //assert
+            Assert.NotNull(FullName);
+        }
+        
+        
+        
     }
 }
