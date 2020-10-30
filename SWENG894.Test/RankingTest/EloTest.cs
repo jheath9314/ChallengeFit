172955ab46be0639c6158b7c912067c910c2424d
jheath9314 <35749255+jheath9314ou@users.noreply.github.com>
2020-10-30 12:54:33 -0600

    Initial commit of code for Elo rating system

---
+++ SWENG894.Test/RankingTest/EloTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xunit;
+using SWENG894.Ranking;
+
+
+namespace SWENG894.Test
+{
+    public class EloTest
+    {
+        [Fact]
+        public void TestVictoryProbability()
+        {
+            var Elo = new Elo();
+
+            int rating1 = 500;
+            int rating2 = 500;
+            double prob = Elo.CalculateP1ProbabilityOfVictory(rating1, rating2);
+            Assert.True(prob == 0.5);
+
+            rating1 = 400;
+            rating2 = 600;
+
+            prob = Elo.CalculateP1ProbabilityOfVictory(rating1, rating2);
+
+            Assert.True(prob < 0.5);
+
+            rating1 = 2000;
+            rating2 = 1999;
+
+            prob = Elo.CalculateP1ProbabilityOfVictory(rating1, rating2);
+            Assert.True(prob > 0.5);
+        }
+
+        [Fact]
+        public void TestRatingSystem()
+        {
+            var Elo = new Elo();
+
+            var P1OriginalRating = 1500;
+            var P2OriginalRating = 500;
+
+            var P1Rating = P1OriginalRating;
+            var P2Rating = P2OriginalRating;
+
+            Elo.CalculateRating(ref P1Rating, ref P2Rating, Elo.Winner.P1);
+
+            //Ensure that when a rating is vastly different, that the expected outcome does not affect ratings
+            Assert.True(P1Rating == P1OriginalRating);
+            Assert.True(P2Rating == P2OriginalRating);
+
+            P1Rating = P1OriginalRating;
+            P2Rating = P2OriginalRating;
+
+            Elo.CalculateRating(ref P1Rating, ref P2Rating, Elo.Winner.P2);
+
+            Assert.True(P1Rating < P1OriginalRating);
+            Assert.True(P2Rating > P2OriginalRating);
+
+            var diff = Math.Abs(P1Rating - P1OriginalRating);
+
+            P1Rating = P1OriginalRating - 500;
+            P2Rating = P2OriginalRating;
+
+            Elo.CalculateRating(ref P1Rating, ref P2Rating, Elo.Winner.P2);
+
+            var diff2 = Math.Abs((P1OriginalRating - 500 - P1Rating));
+
+            //Ensure that the difference between two ratings affects the rating adjustment
+            Assert.True(diff > diff2);
+
+        }
+    }
+}
---
+++ SWENG894/Ranking/Elo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SWENG894.Ranking
+{
+    public class Elo
+    {
+        public enum Winner
+        {
+            P1,
+            P2
+        }
+
+        private int GetKFactor()
+        {
+            return 30;
+        }
+        public void CalculateRating(ref int P1Rating, ref int P2Rating, Winner winner)
+        {
+            var P1ProbWin = CalculateP1ProbabilityOfVictory(P1Rating, P2Rating);
+            var P2ProbWin = 1.0 - P1ProbWin;
+
+            if(winner == Winner.P1)
+            {
+                P1Rating = (int)Math.Round(P1Rating + GetKFactor() * (1 - P1ProbWin));
+                P2Rating = (int)Math.Round(P2Rating + GetKFactor() * (0 - P2ProbWin));
+            }
+            else
+            {
+                P1Rating = (int)Math.Round(P1Rating + GetKFactor() * (0 - P1ProbWin));
+                P2Rating = (int)Math.Round(P2Rating + GetKFactor() * (1 - P2ProbWin));
+            }
+        }
+
+        public double CalculateP1ProbabilityOfVictory(int P1Rating, int P2Rating)
+        {
+            double Probability;
+            double RatingDifference = P2Rating - P1Rating;
+            double expValue = (RatingDifference / 400);
+
+            Probability = 1 / (1 + Math.Pow(10, expValue));
+            return Probability;
+        }
+    }
+}
