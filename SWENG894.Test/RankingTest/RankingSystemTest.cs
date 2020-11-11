80c21b205c0e413230afd2136bcdbd3d5c404600
jheath9314 <35749255+jheath9314ou@users.noreply.github.com>
2020-11-10 20:13:10 -0700

    Added implementation and test code for the ranking system

---
+++ SWENG894.Test/RankingTest/RankingSystemTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using SWENG894.Ranking;
+using Org.BouncyCastle.Asn1.Cmp;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using Org.BouncyCastle.Crypto.Tls;
+
+namespace SWENG894.Test.RankingTest
+{
+    public class RankingSystemTest
+    {
+        [Fact]
+        public void GetClusterCentersTest()
+        {
+            List<int> Ratings = new List<int>();
+            int NumRankings = 5;
+            var cut = new RankingSystem();
+
+            for(int i = 0; i < 20; i++)
+            {
+                Ratings.Add(i);
+                Ratings.Add(i);
+            }
+
+            var Centers = cut.GetInitialClusterCenters(Ratings, NumRankings);
+
+            Assert.True(Centers.Count == 5);
+            Assert.True(Centers.ElementAt(0) == 0);
+            Assert.True(Centers.ElementAt(4) == 32);
+
+            Ratings.Add(21);
+            Ratings.Sort();
+
+            Centers = cut.GetInitialClusterCenters(Ratings, NumRankings);
+            Assert.True(Centers.Count == 5);
+            Assert.True(Centers.ElementAt(0) == 0);
+            Assert.True(Centers.ElementAt(4) == 32);
+
+
+
+        }
+        [Fact]
+        public void GetClosestClusterTest()
+        {
+            var cut = new RankingSystem();
+            List<int> Clusters = new List<int>();
+            Clusters.Add(1);
+            Clusters.Add(7);
+            Clusters.Add(12);
+            Clusters.Add(55);
+            Clusters.Add(99);
+
+            var closest = cut.GetClosestCluster(88, Clusters);
+            Assert.True(closest == 4);
+            closest = cut.GetClosestCluster(9, Clusters);
+            Assert.True(closest == 1);
+            closest = cut.GetClosestCluster(44, Clusters);
+            Assert.True(closest == 3);
+            closest = cut.GetClosestCluster(3, Clusters);
+            Assert.True(closest == 0);
+
+
+        }
+    }
+}
--- SWENG894/Ranking/RankingSystem.cs
+++ SWENG894/Ranking/RankingSystem.cs
@@ -7,5 +7,60 @@ namespace SWENG894.Ranking
 {
     public class RankingSystem
     {
+        public List<int> GenerateRankings(int NumRankings, List<int> Ratings)
+        {
+            Ratings.Sort();
+            return null;
+            //Add the ratings to the struct
+            List<KMeansRating> KMeansRatings = new List<KMeansRating>();
+            for(int i = 0; i < Ratings.Count; i++)
+            {
+                KMeansRating rating;
+                rating.ClosestCluster = 0;
+                rating.Rating = Ratings.ElementAt(0);
+                KMeansRatings.Add(rating);
+            }
+
+            var Centers = GetInitialClusterCenters(Ratings, NumRankings);
+        }
+
+        public int GetClosestCluster(int Rating, List<int> Clusters)
+        {
+            int ClosestCluster = 0;
+            int ClosestClusterDistance = Math.Abs(Rating - Clusters.ElementAt(0));
+
+            for (int i = 1; i < Clusters.Count; i++)
+            { 
+                if(Math.Abs(Rating - Clusters.ElementAt(i)) < ClosestClusterDistance)
+                {
+                    ClosestClusterDistance = Math.Abs(Rating - Clusters.ElementAt(i));
+                    ClosestCluster = i;
+                }
+            }
+
+            return ClosestCluster;
+        }
+
+        public List<int> GetInitialClusterCenters(List<int> Ratings, int NumRankings)
+        {
+            List<int> Centers = new List<int>();
+
+            Double Difference = Ratings.Count / NumRankings;
+
+            for(int i = 0; i < NumRankings; i++)
+            {
+                int val = (int)(Math.Round(i * Difference));
+                Centers.Add(val);
+            }
+
+            return Centers;
+        }
+
+        public struct KMeansRating
+        {
+            public int Rating;
+            public int ClosestCluster;
+        }
     }
+
 }
