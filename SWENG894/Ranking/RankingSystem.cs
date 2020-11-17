73a49f960d109d90a1f2041e34168b4dc36ae971
jheath9314 <35749255+jheath9314ou@users.noreply.github.com>
2020-11-16 18:57:04 -0700

    Added ranking system and tests

--- SWENG894/Ranking/RankingSystem.cs
+++ SWENG894/Ranking/RankingSystem.cs
@@ -7,47 +7,57 @@ namespace SWENG894.Ranking
 {
     public class RankingSystem
     {
-        public List<int> GenerateRankings(int NumRankings, List<int> Ratings)
+        public List<int> GenerateRankings(int NumRankings, List<int> Ratings, int maxIterations)
         {
+            int currentIteration = 0;
+            bool updateOccured = true;
             Ratings.Sort();
-            //return null;
-            //Add the ratings to the struct
+            //Add the ratings to the struct to easily correlate a rating and closest cluster center
             List<KMeansRating> KMeansRatings = new List<KMeansRating>();
             for(int i = 0; i < Ratings.Count; i++)
             {
                 KMeansRating rating = new KMeansRating();
                 rating.ClosestCluster = 0;
-                rating.Rating = Ratings.ElementAt(0);
+                rating.Rating = Ratings.ElementAt(i);
                 KMeansRatings.Add(rating);
             }
 
             List<int> Centers = GetInitialClusterCenters(Ratings, NumRankings);
 
-            //Do outer loops here for repeating the steps
-            //Assign each point to the closest cluster center
-            for(int i = 0; i < KMeansRatings.Count; i++)
+            //Run the algorithm until no updates occur or we've exceeded our max iteration count
+            while (currentIteration < maxIterations && updateOccured)
             {
-                int UpdatedCenter = GetClosestCluster(KMeansRatings.ElementAt(i).Rating, Centers);
-                KMeansRatings.ElementAt(i).ClosestCluster = UpdatedCenter;
+                currentIteration++;
+                updateOccured = false;
+                for (int i = 0; i < KMeansRatings.Count; i++)
+                {
+                    int UpdatedCenter = GetClosestCluster(KMeansRatings.ElementAt(i).Rating, Centers);
+                    KMeansRatings.ElementAt(i).ClosestCluster = UpdatedCenter;
+                }
+
+                //This code could be made more efficient
+                //Calculate the new cluster average
+                for (int i = 0; i < Centers.Count; i++)
+                {
+                    Centers[i] = CalculateAverageForClusterCenter(KMeansRatings, Centers, i);
+                }
+
+                for (int i = 0; i < KMeansRatings.Count; i++)
+                {
+                    int UpdatedCenter = GetClosestCluster(KMeansRatings.ElementAt(i).Rating, Centers);
+                    if(KMeansRatings.ElementAt(i).ClosestCluster != UpdatedCenter)
+                    {
+                        updateOccured = true;
+                    }
+
+                    KMeansRatings.ElementAt(i).ClosestCluster = UpdatedCenter;
+                }
             }
 
-            //This code could be made more efficient
-            //Calculate the new cluster average
-            for(int i = 0; i < KMeansRatings.Count; i++)
-            {
-                Centers[i] = CalculateAverageForClusterCenter(KMeansRatings, Centers, i);
-            }
-
-            //TODO: Check for a change, if it occurs, continue to run the algorithm
-            for (int i = 0; i < KMeansRatings.Count; i++)
-            {
-                int UpdatedCenter = GetClosestCluster(KMeansRatings.ElementAt(i).Rating, Centers);
-                KMeansRatings.ElementAt(i).ClosestCluster = UpdatedCenter;
-            }
-
-
+            return Centers;
         }
 
+        //Returns the closest clusters for a given rating
         public int GetClosestCluster(int Rating, List<int> Clusters)
         {
             int ClosestCluster = 0;
@@ -65,6 +75,7 @@ namespace SWENG894.Ranking
             return ClosestCluster;
         }
 
+        //Given a cluster index, clusters, and ratings, average all the ratings that are closed to a given cluster.
         public int CalculateAverageForClusterCenter(List<KMeansRating> Ratings, List<int> Clusters, int ClusterIndex)
         {
             int count = 0;
@@ -79,7 +90,9 @@ namespace SWENG894.Ranking
                 }
             }
 
-            //Not sure how to handle this case. It probably shouldn't happen
+            //This case is very unlikely to happen. If we find a cluster with no elements tied to it
+            //move on. It will be adjusted as other clusters are adjusted. The case that a cluster center
+            //has no data points tied to it is very unlikely with any significant data set.
             if(count == 0)
             {
                 return Clusters.ElementAt(ClusterIndex);
@@ -87,6 +100,10 @@ namespace SWENG894.Ranking
             return (int)Math.Round((decimal)runningValue / (decimal)count);
         }
 
+        //Returns a roughly uniform division of cluster centers. It's not exact as it starts at 0, but that
+        //is quickly corrected by the execution of the algorithm. This function assumes a sorted rating list. 
+        //The lack of a sorted rating list will not break the function, but results in slower execution and requires
+        //more iterations for convergence.
         public List<int> GetInitialClusterCenters(List<int> Ratings, int NumRankings)
         {
             List<int> Centers = new List<int>();
@@ -102,6 +119,7 @@ namespace SWENG894.Ranking
             return Centers;
         }
 
+        //Helper class for tying a Rating and closest cluster together
         public class KMeansRating
         {
             public int Rating;
